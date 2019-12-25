9dd502868b5608a97b81b1c53db8af33bd261dc7
Jefferson Freitas <ajeffsoft@gmail.com>
2019-12-25 20:28:51 -0300

    Criando classe domain categoria

--- src/main/java/com/ajeff/course/resources/CategoriaResource.java
+++ src/main/java/com/ajeff/course/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.ajeff.course.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ajeff.course.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "Esta OK";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1L, "Informática");
+		Categoria cat2 = new Categoria(2L, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 }
